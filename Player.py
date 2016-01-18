5438ef237a0a6337c1097fb6ef23ed3ac965b97e
Zinoq <0893504@hr.nl>
2016-01-18 10:44:00 +0100

    Improved

--- Player.py
+++ Player.py
@@ -1,4 +1,8 @@
 class Player:
-    def __init__(self, hp, test):
-        self.HP = hp
-        self.Test = test
+    def __init__(self, health, posX, PosY):
+        self.HP = health
+        self.X = posX
+        self.Y = posY
+
+    def decreaseHealth(self, amount):
+        self.HP = self.HP - amount
