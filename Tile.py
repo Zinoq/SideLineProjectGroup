c6a044fceae0576e1beff6caa3ba30305a3c8800
Zinoq <0893504@hr.nl>
2016-01-19 10:53:58 +0100

    Created Tile.py

---
+++ Tile.py
@@ -0,0 +1,11 @@
+class Tile:
+    def __init__(self, position, type):
+        self.Position = Point(x, y)
+        self.Type = type
+
+
+
+class Point:
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
