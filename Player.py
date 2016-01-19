376f897e978da831d1765680d3313f5affa647bd
Zinoq <0893504@hr.nl>
2016-01-19 14:48:29 +0100

    Lots of updates

--- Player.py
+++ Player.py
@@ -3,13 +3,13 @@ import random
 
 class Player:
     def __init__(self, health, position, condition, spawnlocation):
-        self.HP = health
+        self.Health = health
         self.Position = position
         self.Condition = condition
         self.Spawnlocation = spawnlocation #has to be a Point(x,y)
 
     def decreaseHealth(self, amount):
-        self.HP = self.HP - amount
+        self.Health = self.Health - amount
 
     def resetLocation(self):
         self.Position = self.Spawnlocation #is a Point
@@ -21,5 +21,17 @@ class Player:
 # player = Player(100,Point(2,5),100,Point(0,0)) #testing purposes
 # print(player.rollDice())
 
+#Later we should add a function where we can decide how much player we want, and with that we decide how many players we
+#iniate
+Player1 = Player(100, Point(0,0), 15, Point(0,0))
+Player2 = Player(100, Point(260, 0), 15, Point(260,0))
+Player3 = Player(100, Point(0,260), 15, Point(0,260))
+Player4 = Player(100, Point(260,260), 15, Point(260, 260))
+
+# print(Player4.Health)
+#
+# Player4.decreaseHealth(40)
+#
+# print(Player4.Health)  Testing purposes
 
 
