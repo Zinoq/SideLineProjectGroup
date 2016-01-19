ff3282538761655ce9c37d1f0cc170977dfc41dd
Zinoq <0893504@hr.nl>
2016-01-19 14:15:00 +0100

    added spawnlocation

--- Player.py
+++ Player.py
@@ -1,10 +1,12 @@
+from Tile import *
+
 class Player:
-    def __init__(self, health, posX, posY, condition, damage):
+    def __init__(self, health, posX, posY, condition, spawnlocation):
         self.HP = health
         self.X = posX
         self.Y = posY
         self.Condition = condition
-        self.Damage = damage
+        self.Spawnlocation = spawnlocation #has to be a Point(x,y)
 
     def decreaseHealth(self, amount):
         self.HP = self.HP - amount
