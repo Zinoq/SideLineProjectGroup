4399c8d70f9b4f4761561a3444d3e3147293a1b8
Zinoq <0893504@hr.nl>
2016-01-19 11:11:12 +0100

    updated Tile.py

--- Tile.py
+++ Tile.py
@@ -1,11 +1,16 @@
-class Tile:
-    def __init__(self, position, type):
-        self.Position = Point(x, y)
-        self.Type = type
-
-
-
 class Point:
     def __init__(self, x, y):
         self.X = x
         self.Y = y
+
+
+class Tile:
+    def __init__(self, position, type):
+        self.Position = position
+        if type is not "neutral" and type is not "fight" and type is not "spawn":
+            raise Exception("Invalid Type, use \'neutral\', \'fight\' or \'spawn\'")
+        else:
+            self.Type = type
+
+
+# a = Tile(Point(2,5),"spawn")
