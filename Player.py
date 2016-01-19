0b26b9d34cc6e33fcebeb175f74bf61dc92f6cee
Zinoq <0893504@hr.nl>
2016-01-19 14:22:41 +0100

    added spawnlocation, changed location

--- Player.py
+++ Player.py
@@ -1,13 +1,15 @@
 from Tile import *
 
 class Player:
-    def __init__(self, health, posX, posY, condition, spawnlocation):
+    def __init__(self, health, position, condition, spawnlocation):
         self.HP = health
-        self.X = posX
-        self.Y = posY
+        self.Position = position
         self.Condition = condition
         self.Spawnlocation = spawnlocation #has to be a Point(x,y)
 
     def decreaseHealth(self, amount):
         self.HP = self.HP - amount
+
+    def resetLocation(self):
+        self.Position = self.Spawnlocation #is a Point
 
