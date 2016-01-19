92285c802090d55d1b25d0a60edcb08c984abf83
Zinoq <0893504@hr.nl>
2016-01-19 10:46:52 +0100

    Updated player.py

--- Player.py
+++ Player.py
@@ -1,8 +1,9 @@
 class Player:
-    def __init__(self, health, posX, posY):
+    def __init__(self, health, posX, posY, condition):
         self.HP = health
         self.X = posX
         self.Y = posY
+        self.Condition = condition
 
     def decreaseHealth(self, amount):
         self.HP = self.HP - amount
