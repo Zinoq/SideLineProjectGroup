30a629dfcc67a37e2fac3bb8222328f56561996e
Zinoq <0893504@hr.nl>
2016-01-21 14:03:26 +0100

    Updated StartMenu

--- Startmenu.py
+++ Startmenu.py
@@ -1,5 +1,6 @@
 __author__ = 'Tristan & Zino'
 import pygame
+import time
 from Game import game
 
 
@@ -41,11 +42,19 @@ class Button:
         else:
             return False
 
-
+#Start
 button1 = Button("START!", green, (180, 550, 250, 75),((180+125), (550+(75/2))))
+
+#Exit
 button2 = Button("EXIT", red,(850, 550, 250, 75), ((850+125), (550+(75/2))))
+
+#Background 1
 unscaled_bg = pygame.image.load("assets\\title1.png")
 bg = pygame.transform.scale(unscaled_bg,size)
+
+#Background 2
+unscaled_bg2 = pygame.image.load("assets\\title2.png")
+bg2 = pygame.transform.scale(unscaled_bg2,size)
 
 def main():
     while True:
@@ -59,10 +68,12 @@ def main():
         button1.DrawButton()
         button2.DrawButton()
 
-
+        #Start button
         if 180+250 > mouse[0] > 180 and 550+75 > mouse [1] > 550:
             pygame.draw.rect(screen, brightgreen,(180, 550, 250, 75))
             if pygame.mouse.get_pressed()[0]:
+                screen.blit(bg2,(0,0))
+                time.sleep(2)
                 game()
 
         else:
@@ -71,6 +82,8 @@ def main():
         textRect.center = ((180+125), (550+(75/2)))
         screen.blit(textSurf, textRect)
 
+
+        #Exit button
         if 850+250 > mouse[0] > 850 and 550+75 > mouse[1] > 550:
             pygame.draw.rect(screen, brightred,(850, 550, 250, 75))
             if pygame.mouse.get_pressed()[0]:
