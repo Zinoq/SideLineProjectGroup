ee75c791b61055a2a41ed1e31c106fdffd8ccf03
Zinoq <0893504@hr.nl>
2016-01-25 11:28:57 +0100

    gotta test this later

--- Turn.py
+++ Turn.py
@@ -1,10 +1,8 @@
-from Player import *
 from Game import *
 
 def changeTurn():
     o = 1
-    rolled = players[i].rollDice()
-    print(rolled)
+    rolled = players[o].rollDice()
     return rolled
 
 
