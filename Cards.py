a1cf2eeb516f790e4fd33f24455cb9af2bee6169
Zinoq <0893504@hr.nl>
2016-02-01 10:23:21 +0100

    Added card class, cards and their textures

---
+++ Cards.py
@@ -0,0 +1,11 @@
+
+class Card:
+    def __init__(self, name, val1, val2, val3, val4, val5, val6):
+        self.Name = name
+        self.dmg1 = val1
+        self.dmg2 = val2
+        self.dmg3 = val3
+        self.dmg4 = val4
+        self.dmg5 = val5
+        self.dmg6 = val6
+
